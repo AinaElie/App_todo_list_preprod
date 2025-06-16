44e51b349954f4a6b4ae48329387c5b56d68ff19
AinaElie <elie.anjarafanomezana@gmail.com>
2025-06-16 08:18:03 +0300

    chore: new logo

--- app/page.tsx
+++ app/page.tsx
@@ -28,13 +28,51 @@ export default function Home() {
           {loading ? (
             <Loading />
           ) : (
-            <Link
-              onClick={() => setLoading(true)}
-              href={"/login"}
-              className="bg-black transition-all text-white font-light text-sm px-4 py-2 border border-black rounded-md hover:bg-transparent hover:text-black"
-            >
-              Connexion
-            </Link>
+            <div className="flex justify-center items-center">
+              <Link
+                onClick={() => setLoading(true)}
+                href={"/login"}
+                className="bg-black group flex justify-center items-center transition-all text-white font-light text-sm mr-4 px-4 py-3 border border-black rounded-md hover:bg-white"
+              >
+                <p className="group-hover:text-black">Connexion</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="23"
+                  height="23"
+                  viewBox="0 0 23 23"
+                  fill="none"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="stroke-white ml-4 group-hover:stroke-black"
+                >
+                  <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
+                  <polyline points="10 17 15 12 10 7"></polyline>
+                  <line x1="15" x2="3" y1="12" y2="12"></line>
+                </svg>
+              </Link>
+              <Link
+                onClick={() => setLoading(true)}
+                href={"/home"}
+                className="bg-black group flex justify-center items-center transition-all text-white font-light text-sm ml-4 px-4 py-3 border border-black rounded-md hover:bg-transparent"
+              >
+                <p className="group-hover:text-black">Discovery</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="23"
+                  height="23"
+                  viewBox="0 0 23 23"
+                  fill="none"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="stroke-white ml-4 group-hover:stroke-black"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="m12 5 7 7-7 7"></path>
+                </svg>
+              </Link>
+            </div>
           )}
         </div>
       </div>
