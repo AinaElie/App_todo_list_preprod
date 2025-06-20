086ac84056eaae5a3e98daab9ec92600e34060eb
AinaElie <elie.anjarafanomezana@gmail.com>
2025-06-20 16:40:23 +0300

    feat: endpoint /api/users

---
+++ app/api/users/route.ts
@@ -0,0 +1,13 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const users = await prisma.user.findMany();
+        return new NextResponse(JSON.stringify(users));
+    }   catch (error) {
+        return new NextResponse(JSON.stringify({ error: "Repository error" }),
+            { status: 500 }
+        )
+    }
+}
