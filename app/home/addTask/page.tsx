a7ec7f8739b227c9bf434c39fbcee5d9ed3ea796
AinaElie <elie.anjarafanomezana@gmail.com>
2025-07-07 13:28:54 +0300

    feat: page as add task

--- app/home/addTask/page.tsx
+++ app/home/addTask/page.tsx
@@ -1,5 +1,13 @@
+"use client"
+
+import AddTask from "@/app/components/AddTask";
+
 export default function Page() {
+
   return (
-    <div className="w-full h-screen flex justify-center items-center">AddTask</div>
-  )
+    <div className="w-full pt-32 pl-28">
+      {/* <h1>Add new Task</h1> */}
+      <AddTask />
+    </div>
+  );
 }
