1772547bfbedb5dbda7a22c23cdf7814c4cca72b
AinaElie <elie.anjarafanomezana@gmail.com>
2025-07-11 12:33:49 +0300

    feat: edpoint get user by id and email

---
+++ app/api/users/search/route.ts
@@ -0,0 +1,33 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  try {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get("email");
+
+    if (!query) {
+      return NextResponse.json({ message: "Email not found" }, { status: 404 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: query,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ data: user }, { status: 200 });
+  } catch (error) {
+        return new NextResponse(
+            JSON.stringify({ 
+                error: "Internal server error",
+                details: error instanceof Error ? error.message : String(error)
+            }),
+            { status: 500 }
+        );
+    }
+}
--- app/home/addTask/page.tsx
+++ app/home/addTask/page.tsx
@@ -1,9 +1,8 @@
-"use client"
+"use client";
 
 import CrudTask from "@/app/components/CrudTask";
 
 export default function Page() {
-
   return (
     <div className="w-full pt-32 pl-28">
       {/* <h1>Add new Task</h1> */}
