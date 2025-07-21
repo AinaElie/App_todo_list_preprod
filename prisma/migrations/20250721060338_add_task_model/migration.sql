4aa017b84179c68fde21e650d0ffa8c6929d5afe
AinaElie <elie.anjarafanomezana@gmail.com>
2025-07-21 09:04:23 +0300

    feat: model task database

---
+++ prisma/migrations/20250721060338_add_task_model/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "TaskPriority" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "description" TEXT,
+    "priority" "TaskPriority" NOT NULL DEFAULT 'MEDIUM',
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
