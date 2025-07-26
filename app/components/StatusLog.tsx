df2c8a81646db8cad2fe643cfe82af0f30b35785
AinaElie <elie.anjarafanomezana@gmail.com>
2025-07-26 09:50:10 +0300

    feat: log message

---
+++ app/components/StatusLog.tsx
@@ -0,0 +1,71 @@
+export default function StatusLog({
+  status,
+  message,
+}: {
+  status: number;
+  message: string;
+}) {
+  if (status == 201) {
+    return (
+      <div className="w-3/5 py-2 px-4 shadow-lg shadow-stone-200 mx-4 rounded-lg text-center text-sm text-green-700 border border-green-700 flex justify-around items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="19"
+          height="19"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="stroke-green-700"
+        >
+          <path d="M21.801 10A10 10 0 1 1 17 3.335"></path>
+          <path d="m9 11 3 3L22 4"></path>
+        </svg>
+        <p>{message}</p>
+      </div>
+    );
+  } else if (status == 404) {
+    return (
+      <div className="w-2/5 py-2 px-4 shadow-lg shadow-stone-200 mx-4 rounded-lg text-center text-sm text-red-600 border border-text-red-600 flex justify-around items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="19"
+          height="19"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="stroke-red-600"
+        >
+          <circle cx="12" cy="12" r="10"></circle>
+          <path d="m15 9-6 6"></path>
+          <path d="m9 9 6 6"></path>
+        </svg>
+        <p>Not task added</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="w-1/2 py-2 px-4 shadow-lg shadow-stone-200 mx-4 rounded-lg text-center text-sm text-red-600 border border-text-red-600 flex justify-around items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="19"
+          height="19"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="stroke-red-600"
+        >
+          <circle cx="12" cy="12" r="10"></circle>
+          <path d="m15 9-6 6"></path>
+          <path d="m9 9 6 6"></path>
+        </svg>
+        <p>{message}</p>
+      </div>
+    );
+  }
+}
--- app/components/crudTask/RudTask.tsx
+++ app/components/crudTask/RudTask.tsx
@@ -3,18 +3,22 @@
 import { useState } from "react";
 import CardTask from "./CardTask";
 import { Task } from "./task";
+import StatusLog from "../StatusLog";
 
 export default function RudTask({ tasks }: { tasks: Task[] }) {
   const [isLoading, setLoading] = useState(false);
+  const [log, setLog] = useState("");
+  const [status, setStatus] = useState(404);
 
   async function create() {
     setLoading(true);
     try {
       const resUser = await fetch("/api/users");
-      
+
       if (resUser.status != 200) {
         const data = await resUser.json();
-        console.log(data);
+        setLog(data.message);
+        setStatus(resUser.status);
       }
       const dataUser = await resUser.json();
 
@@ -27,14 +31,19 @@ export default function RudTask({ tasks }: { tasks: Task[] }) {
       });
 
       if (req.status === 201) {
-        console.log("Tasks added");
+        const res = await req.json();
+        setLog(res.message);
+        setStatus(req.status);
       } else {
         const res = await req.json();
-        console.error("Error", res.error);
+        setLog(res.message);
+        setStatus(req.status);
         console.error("Error", res.details);
       }
     } catch (error) {
-      console.error("Erreur réseau ou serveur :", error);
+      setLog(`${error}`);
+      setStatus(500);
+      console.error("Error servor :", error);
     } finally {
       setLoading(false);
     }
@@ -42,32 +51,37 @@ export default function RudTask({ tasks }: { tasks: Task[] }) {
 
   return (
     <div className="ml-8 py-4 px-8 h-full w-full flex flex-wrap gap-8 relative">
-      <button
-        onClick={create}
-        className="group absolute -top-9 right-4 border border-indigo-700 py-2 px-4 cursor-pointer flex bg-indigo-700 hover:bg-white rounded-lg transition-all"
-      >
-        {" "}
-        {isLoading ? (
-          <p className="group-hover:text-indigo-700 text-white ">...</p>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="stroke-white group-hover:stroke-indigo-700"
-          >
-            <path d="M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z"></path>
-            <path d="M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7"></path>
-            <path d="M7 3v4a1 1 0 0 0 1 1h7"></path>
-          </svg>
-        )}
-        <p className="group-hover:text-indigo-700 text-white pl-2">Save all</p>
-      </button>
+      <div className="absolute -top-9 right-4 flex justify-around items-center px-4 w-1/2">
+        <StatusLog status={status} message={log} />
+        <button
+          onClick={create}
+          className="group border border-indigo-700 py-2 px-4 cursor-pointer flex bg-indigo-700 hover:bg-white rounded-lg transition-all"
+        >
+          {" "}
+          {isLoading ? (
+            <p className="group-hover:text-indigo-700 text-white pr-2">...</p>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="stroke-white group-hover:stroke-indigo-700"
+            >
+              <path d="M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z"></path>
+              <path d="M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7"></path>
+              <path d="M7 3v4a1 1 0 0 0 1 1h7"></path>
+            </svg>
+          )}
+          <p className="group-hover:text-indigo-700 text-white pl-2">
+            Save all
+          </p>
+        </button>
+      </div>
       {tasks.length != 0
         ? tasks.map((task) => <CardTask key={task.id} task={task} />)
         : "Not task at the moment"}
